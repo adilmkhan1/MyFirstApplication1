1752455489f90ef595696a864d7853945c779195
AdilMateenKhan1 <lucky6873@gmail.com>
2017-01-18 22:57:15 +0530

    Used Glide Image Library inplace of Picasso

--- app/src/main/java/com/example/amk/myfirstapplication/CustomAdapter.java
+++ app/src/main/java/com/example/amk/myfirstapplication/CustomAdapter.java
@@ -12,6 +12,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -66,9 +68,16 @@ public class CustomAdapter extends ArrayAdapter<com.example.amk.myfirstapplicati
         urlName.loadUrl(item.url);
 */
 
-        Picasso.with(convertView.getContext())
+        Glide.with(convertView.getContext())
                 .load(item.urlToImage)
+                .thumbnail(0.5f)
+                .crossFade()
                 .into(urlToImageName);
+
+
+        /*Picasso.with(convertView.getContext())
+                .load(item.urlToImage)
+                .into(urlToImageName);*/
 
 
         //new DownloadImageTask(urlToImageName).execute(item.urlToImage);
